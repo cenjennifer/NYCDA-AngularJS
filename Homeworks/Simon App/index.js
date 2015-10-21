627ebddb05d792a48515e5fe824b10456f2495f0
Jennifer Cen <cenjennifer@gmail.com>
2015-10-20 23:42:07 -0400

    Updating Homework

---
+++ Homeworks/Simon App/index.js
@@ -0,0 +1,17 @@
+angular.module('MyApp', [])
+
+.controller('MyController', function(){
+var self = this;
+
+self.startGame = function(){
+	self.gameMessage = true;
+};
+self.changeColor = 
+self.quarterClicked = function(){
+
+};
+
+
+
+
+});
